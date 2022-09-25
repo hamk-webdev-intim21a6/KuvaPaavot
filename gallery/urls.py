9480d4ef65e577dd169f536711b7144958daf686
Jan-Lehtinen <jan.lehtinen@student.hamk.fi>
2022-09-25 21:58:18 +0300

    initial 2.0

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 from . import views
 
+app_name = 'gallery'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', display_images, name = 'display_images'),
+    path('image_upload', image_upload, name = 'image_upload'),
+    path('success', success, name = 'success'),
+    path('', views.index, name='index')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Post
+from .forms import UploadForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the gallery index.")
+
+def display_images(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        return render(request, 'gallery/index.html', {'posts' : posts})
+
+def image_upload(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:success')
+    else:
+        form = UploadForm()
+    return render(request, 'gallery/upload.html', {'form' : form})
+
+
+def success(request):
+    return render(request, 'gallery/success.html', {})
