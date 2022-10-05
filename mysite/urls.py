597281919f0e006a4ed1d723fb13cf97a2d51df9
Petri M <petri.malmstrom@gmail.com>
2022-10-05 21:13:09 +0300

    urls updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('gallery/', include('gallery.urls')),
+    path('', include('gallery.urls')),
     path('admin/', admin.site.urls),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'))
