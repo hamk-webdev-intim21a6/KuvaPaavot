a77c6d64e3b06c3058be18cff5b6abc487747989
Jan-Lehtinen <jan.lehtinen@student.hamk.fi>
2022-10-06 10:52:39 +0300

    Registration funktional

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -22,3 +23,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You are now logged out."))
     return redirect('gallery:index')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration successful."))
+            return redirect('gallery:index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {'form':form,})
